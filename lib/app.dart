d187686b8dd972d43f9e0485b6d157b170c0cf63
EgorK0rshun <egork@digitalchemy.us>
2023-02-02 12:46:15 +0300

    add wireframe

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:text_editor/ui/main_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter quill test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
